7f55fe2932465150d55e09392635f0068d1fd8ce
zaru <sakuraba@basicinc.jp>
2016-09-29 12:49:35 +0900

    modify the allow anyone to connect to ActionCable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # config.action_cable.allowed_request_origins = [ 'http://actioncable.example.com' ]
+  config.action_cable.disable_request_forgery_protection = true
 end

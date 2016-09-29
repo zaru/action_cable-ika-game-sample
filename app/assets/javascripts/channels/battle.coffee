8c95b74728c971508ebaf82eee14fe50b6cf4af3
zaru <sakuraba@basicinc.jp>
2016-09-29 13:05:55 +0900

    add window click event

--- app/assets/javascripts/channels/battle.coffee
+++ app/assets/javascripts/channels/battle.coffee
@@ -6,7 +6,7 @@ App.battle = App.cable.subscriptions.create "BattleChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    # Called when there's incoming data on the websocket for this channel
+    console.log data
 
-  attack: ->
-    @perform 'attack'
+  attack: (position) ->
+    @perform 'attack', position: position
--- app/channels/battle_channel.rb
+++ app/channels/battle_channel.rb
@@ -1,13 +1,14 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class BattleChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "battle_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def attack
+  def attack(position)
+    ActionCable.server.broadcast "battle_channel", message: position
   end
 end
